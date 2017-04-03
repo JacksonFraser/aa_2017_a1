759149c8e1157aaf3668bec1797ef693f35fa3b0
Jackson Fraser <s3529589@student.rmit.edu.au>
2017-04-04 09:49:22 +1000

    _

--- nearestNeighSearch/src/nearestNeigh/NaiveNN.java
+++ nearestNeighSearch/src/nearestNeigh/NaiveNN.java
@@ -20,18 +20,35 @@ public class NaiveNN implements NearestNeigh{
     @Override
     public List<Point> search(Point searchTerm, int k) {
     	List<Point> result = new ArrayList<Point>();
-        for(int i = 0; i < nodesList.size(); ++i) {
-        	if(nodesList.get(i) == searchTerm) {
-        		result.add(nodesList.get(i));
-        	}
-        }
+    	Point p = new Point();
+    	List<Point> correctCat = new ArrayList<Point>();
+    	for(int i = 0; i < nodesList.size(); ++i) {
+    		if(searchTerm.cat.equals(nodesList.get(i).cat)){
+    			correctCat.add(nodesList.get(i));
+    		}
+    	}
+    	for(int i = 0; i < correctCat.size(); ++i) {
+    		p = nodesList.get(i);
+    		for(int j = 1; j < correctCat.size(); ++j) {
+    			if(searchTerm.distTo(correctCat.get(j)) < searchTerm.distTo(p)) {
+    				p = correctCat.get(j);
+    				
+    			}
+    		}
+    		result.add(p);
+    			
+    		
+    		
+    		if(result.size() == k)
+    			return result;
+    	}
+    	
         return result;
     }
-
     @Override
     public boolean addPoint(Point point) {
     	for(int i = 0; i < nodesList.size(); ++i) {
-        	if(nodesList.get(i) == point) {
+        	if(nodesList.get(i).id.equals(point.id)) {
         		return false;
         	}
     	}
@@ -42,19 +59,19 @@ public class NaiveNN implements NearestNeigh{
     @Override
     public boolean deletePoint(Point point) {
     	for(int i = 0; i < nodesList.size(); ++i) {
-        	if(nodesList.get(i).cat == point.cat && nodesList.get(i).id == point.id
-        			&& nodesList.get(i).lat == point.lat && nodesList.get(i).lon == point.lon) {
+        	if(nodesList.get(i).id.equals(point.id)) {
         		nodesList.remove(i);
         		return true;
         	}
     	}
         return false;
+  
     }
 
     @Override
     public boolean isPointIn(Point point) {
     	for(int i = 0; i < nodesList.size(); ++i) {
-        	if(nodesList.get(i) == point) {
+        	if(nodesList.get(i).id.equals(point.id)) {
         		return true;
         	}
     	}
