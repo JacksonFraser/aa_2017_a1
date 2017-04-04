62a5b80ab31c46cdd36348c6763ba74959676818
Jackson Fraser <s3529589@student.rmit.edu.au>
2017-04-04 10:53:16 +1000

    Completed search

--- nearestNeighSearch/src/nearestNeigh/NaiveNN.java
+++ nearestNeighSearch/src/nearestNeigh/NaiveNN.java
@@ -4,78 +4,82 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This class is required to be implemented.  Naive approach implementation.
+ * This class is required to be implemented. Naive approach implementation.
  *
  * @author Jeffrey, Youhan
  */
-public class NaiveNN implements NearestNeigh{
+public class NaiveNN implements NearestNeigh {
 	List<Point> nodesList = new ArrayList<Point>();
-    @Override
-    public void buildIndex(List<Point> points) {
-        for(int i = 0; i < points.size(); ++i) {
-        	nodesList.add(points.get(i));
-        }
-    }
 
-    @Override
-    public List<Point> search(Point searchTerm, int k) {
-    	List<Point> result = new ArrayList<Point>();
-    	Point p = new Point();
-    	List<Point> correctCat = new ArrayList<Point>();
-    	for(int i = 0; i < nodesList.size(); ++i) {
-    		if(searchTerm.cat.equals(nodesList.get(i).cat)){
-    			correctCat.add(nodesList.get(i));
-    		}
-    	}
-    	for(int i = 0; i < correctCat.size(); ++i) {
-    		p = nodesList.get(i);
-    		for(int j = 1; j < correctCat.size(); ++j) {
-    			if(searchTerm.distTo(correctCat.get(j)) < searchTerm.distTo(p)) {
-    				p = correctCat.get(j);
-    				
-    			}
-    		}
-    		result.add(p);
-    			
-    		
-    		
-    		if(result.size() == k)
-    			return result;
-    	}
-    	
-        return result;
-    }
-    @Override
-    public boolean addPoint(Point point) {
-    	for(int i = 0; i < nodesList.size(); ++i) {
-        	if(nodesList.get(i).id.equals(point.id)) {
-        		return false;
-        	}
-    	}
-        nodesList.add(point);
-        return true;
-    }
+	@Override
+	public void buildIndex(List<Point> points) {
+		for (int i = 0; i < points.size(); ++i) {
+			nodesList.add(points.get(i));
+		}
+	}
 
-    @Override
-    public boolean deletePoint(Point point) {
-    	for(int i = 0; i < nodesList.size(); ++i) {
-        	if(nodesList.get(i).id.equals(point.id)) {
-        		nodesList.remove(i);
-        		return true;
-        	}
-    	}
-        return false;
-  
-    }
+	@Override
+	public List<Point> search(Point searchTerm, int k) {
+		List<Point> result = new ArrayList<Point>();
+		Point p = new Point();
+		List<Point> correctCat = new ArrayList<Point>();
+		for (int i = 0; i < nodesList.size(); ++i) {
+			if (searchTerm.cat.equals(nodesList.get(i).cat)) {
+				correctCat.add(nodesList.get(i));
+			}
+		}
+		for (int i = 1; i < correctCat.size(); ++i) {
+			p = correctCat.get(i);
+			int j = i;
+			while (j > 0 && searchTerm.distTo(correctCat.get(j - 1)) > searchTerm.distTo(p)) {
+				correctCat.set(j, correctCat.get(j - 1));
+				j--;
 
-    @Override
-    public boolean isPointIn(Point point) {
-    	for(int i = 0; i < nodesList.size(); ++i) {
-        	if(nodesList.get(i).id.equals(point.id)) {
-        		return true;
-        	}
-    	}
-        return false;
-    }
+			}
+			correctCat.set(j, p);
+		}
+		for(int i = 0; i < k; ++i) {
+			result.add(correctCat.get(i));
+		}
+		System.out.println(result.size());
+		System.out.println(k);
+
+
+	return result;
+
+	}
+
+	@Override
+	public boolean addPoint(Point point) {
+		for (int i = 0; i < nodesList.size(); ++i) {
+			if (nodesList.get(i).id.equals(point.id)) {
+				return false;
+			}
+		}
+		nodesList.add(point);
+		return true;
+	}
+
+	@Override
+	public boolean deletePoint(Point point) {
+		for (int i = 0; i < nodesList.size(); ++i) {
+			if (nodesList.get(i).id.equals(point.id)) {
+				nodesList.remove(i);
+				return true;
+			}
+		}
+		return false;
+
+	}
+
+	@Override
+	public boolean isPointIn(Point point) {
+		for (int i = 0; i < nodesList.size(); ++i) {
+			if (nodesList.get(i).id.equals(point.id)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
